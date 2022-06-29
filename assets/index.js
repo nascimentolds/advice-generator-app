dd3448d0bd0c17443e91f0817a4b194498424a26
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2022-06-28 22:40:14 -0300

    Javascript finished

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,13 @@
-/*const url = "https://api.adviceslip.com/advice";
-
-function getAdvice() {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data.slip))
-    .catch((error) => console.log(error));
+const showData = (result) => {
+  document.querySelector("#id").innerHTML = "ADVICE # " + result.id;
+  document.querySelector("#advice").innerHTML = result.advice;
 }
 
-getAdvice();
-/*adviceId.textContent = JSON.stringify(data.id))*/
+function advice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => showData(data.slip))
+    .catch((err) => console.log(err));
+}
 
-fetch("https://api.adviceslip.com/advice")
-  .then((response) => response.json())
-  .then((data) => {
-    adviceId.textContent = data.slip.id;
-    advice.textContent = data.slip.advice;
-  })
-  .catch((err) => console.log(err));
+advice()
